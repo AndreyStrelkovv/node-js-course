6dd9277bfed466808fc013f7f0085f6bf0a849be
Andrey Strelkov <andrey.strelkov@so.energy>
2023-01-26 18:07:13 +0000

    mongodb users collection + prettier

--- mongodb/app.js
+++ mongodb/app.js
@@ -1,37 +1,37 @@
-const path = require("path");
+const path = require("path")
 
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require("express")
+const bodyParser = require("body-parser")
 
-const errorController = require("./controllers/error");
-const mongoConnect = require("./util/database").mongoConnect;
+const errorController = require("./controllers/error")
+const mongoConnect = require("./util/database").mongoConnect
 
-const app = express();
+const app = express()
 
-app.set("view engine", "ejs");
-app.set("views", "views");
+app.set("view engine", "ejs")
+app.set("views", "views")
 
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
+const adminRoutes = require("./routes/admin")
+const shopRoutes = require("./routes/shop")
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, "public")))
 
 app.use((req, res, next) => {
-  // User.findById(1)
-  //   .then(user => {
-  //     req.user = user;
-  //     next();
-  //   })
-  //   .catch(err => console.log(err));
-  next();
-});
+  User.findById("63d2c09139d591af29af18b4")
+    .then((user) => {
+      req.user = user
+      next()
+    })
+    .catch((err) => console.log(err))
+  next()
+})
 
-app.use("/admin", adminRoutes);
-app.use(shopRoutes);
+app.use("/admin", adminRoutes)
+app.use(shopRoutes)
 
-app.use(errorController.get404);
+app.use(errorController.get404)
 
 mongoConnect(() => {
-  app.listen(3000);
-});
+  app.listen(3000)
+})
--- mongodb/models/product.js
+++ mongodb/models/product.js
@@ -1,72 +1,72 @@
-const mongodb = require("mongodb");
-const getDb = require("../util/database").getDb;
+const mongodb = require("mongodb")
+const getDb = require("../util/database").getDb
 
-const ObjectId = mongodb.ObjectId;
+const ObjectId = mongodb.ObjectId
 
 class Product {
   constructor(title, price, description, imageUrl, id) {
-    this.title = title;
-    this.price = price;
-    this.description = description;
-    this.imageUrl = imageUrl;
+    this.title = title
+    this.price = price
+    this.description = description
+    this.imageUrl = imageUrl
     // console.log(id);
-    this._id = id ? new ObjectId(id) : undefined;
+    this._id = id ? new ObjectId(id) : undefined
   }
 
   save() {
-    const db = getDb();
-    let dbOp;
+    const db = getDb()
+    let dbOp
 
     // console.log("idid:" + this._id);
     if (this._id) {
       // update existing product
       dbOp = db
         .collection("products")
-        .updateOne({ _id: this._id }, { $set: this });
+        .updateOne({ _id: this._id }, { $set: this })
     } else {
       // console.log("here");
       // insert new product
-      dbOp = db.collection("products").insertOne(this);
+      dbOp = db.collection("products").insertOne(this)
     }
     return dbOp
-      .then(result => {
-        console.log(result);
+      .then((result) => {
+        console.log(result)
       })
-      .catch(err => console.log(err));
+      .catch((err) => console.log(err))
   }
 
   static fetchAll() {
-    const db = getDb();
+    const db = getDb()
     return db
       .collection("products")
       .find()
       .toArray()
-      .then(products => {
-        return products;
+      .then((products) => {
+        return products
       })
-      .catch(err => console.log(err));
+      .catch((err) => console.log(err))
   }
 
   static findById(prodId) {
-    const db = getDb();
+    const db = getDb()
     return db
       .collection("products")
       .find({ _id: new ObjectId(prodId) })
       .next()
-      .then(product => {
+      .then((product) => {
         // console.log(product);
-        return product;
-      });
+        return product
+      })
   }
 
   static deleteById(prodId) {
-    const db = getDb();
+    const db = getDb()
     return db
       .collection("products")
       .deleteOne({ _id: new ObjectId(prodId) })
-      .then(result => console.log("deleted"))
-      .catch(err => console.log(err));
+      .then((result) => console.log("deleted"))
+      .catch((err) => console.log(err))
   }
 }
 
-module.exports = Product;
+module.exports = Product

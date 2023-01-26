2d7d992386377fe4c318cc59e44f8b015ce9471e
Andrey Strelkov <andrey.strelkov@so.energy>
2023-01-26 17:43:30 +0000

    mongodb deleting and editing products

--- mongodb/models/product.js
+++ mongodb/models/product.js
@@ -1,18 +1,34 @@
+const mongodb = require("mongodb");
 const getDb = require("../util/database").getDb;
 
+const ObjectId = mongodb.ObjectId;
+
 class Product {
-  constructor(title, price, description, imageUrl) {
+  constructor(title, price, description, imageUrl, id) {
     this.title = title;
     this.price = price;
     this.description = description;
     this.imageUrl = imageUrl;
+    // console.log(id);
+    this._id = id ? new ObjectId(id) : undefined;
   }
 
   save() {
     const db = getDb();
-    return db
-      .collection("products")
-      .insertOne(this)
+    let dbOp;
+
+    // console.log("idid:" + this._id);
+    if (this._id) {
+      // update existing product
+      dbOp = db
+        .collection("products")
+        .updateOne({ _id: this._id }, { $set: this });
+    } else {
+      // console.log("here");
+      // insert new product
+      dbOp = db.collection("products").insertOne(this);
+    }
+    return dbOp
       .then(result => {
         console.log(result);
       })
@@ -26,9 +42,29 @@ class Product {
       .find()
       .toArray()
       .then(products => {
-        console.log(products);
         return products;
       })
+      .catch(err => console.log(err));
+  }
+
+  static findById(prodId) {
+    const db = getDb();
+    return db
+      .collection("products")
+      .find({ _id: new ObjectId(prodId) })
+      .next()
+      .then(product => {
+        // console.log(product);
+        return product;
+      });
+  }
+
+  static deleteById(prodId) {
+    const db = getDb();
+    return db
+      .collection("products")
+      .deleteOne({ _id: new ObjectId(prodId) })
+      .then(result => console.log("deleted"))
       .catch(err => console.log(err));
   }
 }
